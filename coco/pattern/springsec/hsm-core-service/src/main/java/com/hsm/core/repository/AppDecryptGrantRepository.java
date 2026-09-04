e3aba91cf0b0e16d945c5f4fedacdf686bd05733
cobbleacademy <cobbleacademy@users.noreply.github.com>
2026-09-03 21:24:35 -0500

    Delete coco/pattern/springsec/hsm-core-service/src/main/java/com/hsm/core/repository/AppDecryptGrantRepository.java

--- coco/pattern/springsec/hsm-core-service/src/main/java/com/hsm/core/repository/AppDecryptGrantRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hsm.core.repository;
-
-import com.hsm.core.model.AppDecryptGrant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppDecryptGrantRepository extends JpaRepository<AppDecryptGrant, AppDecryptGrant.Key> {
-}
